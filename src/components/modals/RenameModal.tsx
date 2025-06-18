ae6ec5fdffa813f9605ce052680e24341ddb4849
Africk-Hunter <hunterafrick@gmail.com>
2025-06-17 21:13:59 -0700

    Update RenameModal.tsx
    - Made it so when renaming, text is more 'editable' than just replaced. This will make renaming ideas for intuitive.

--- src/components/modals/RenameModal.tsx
+++ src/components/modals/RenameModal.tsx
@@ -1,11 +1,18 @@
 import { useIdeaContext } from "../../context/IdeaContext";
 import { updateIdeaName, updateIdeaNameInFirebase } from "../../utilities";
+import { useEffect as reactUseEffect, useEffect } from "react";
 
 
 function RenameModal() {
 
-    const { rootId, setRootName, renameModalOpen, setRenameModalOpen, modalContent, setModalContent, setNewIdeaSwitch } = useIdeaContext();
+    const { rootId, rootName, setRootName, renameModalOpen, setRenameModalOpen, modalContent, setModalContent, setNewIdeaSwitch } = useIdeaContext();
 
+    useEffect(() => {
+        if (renameModalOpen) {
+            setModalContent(rootName);
+        }
+    }, [renameModalOpen, rootName, setModalContent]);
+    
     function handleIdeaRename(rootID: number, newName: string) {
         updateIdeaNameInFirebase(rootID, newName).then(() => {
             setRootName(newName);
@@ -15,12 +22,20 @@ function RenameModal() {
         });
     }
 
+    //setModalContent(rootName) neecd to call this function on instantiation. so its set to that when the user accesses it.
     return (
         <>
             {renameModalOpen ?
                 <section className="overlay">
                     <div className="modal neobrutal">
-                        <textarea autoFocus={true} maxLength={100} className="ideaContent neobrutal-input" placeholder='Rename Idea...' onChange={(e) => setModalContent(e.target.value)}></textarea>
+                        <textarea
+                            autoFocus={true}
+                            maxLength={100}
+                            className="ideaContent neobrutal-input"
+                            placeholder='Rename Idea...'
+                            value={modalContent} //This needs to be set to something called 'ranameIdeaName.' Doing just root wont work if a rename button is to be added.
+                            onChange={(e) => setModalContent(e.target.value)}
+                        ></textarea>
                         <section className="modalButtons">
                             <button className="modalButton cancel neobrutal-button" onClick={() => setRenameModalOpen(false)}>Cancel</button>
                             <button className="modalButton continue neobrutal-button" onClick={() => { handleIdeaRename(rootId, modalContent); setRenameModalOpen(false); setNewIdeaSwitch(prev => !prev) }}>Rename</button>
@@ -32,4 +47,4 @@ function RenameModal() {
     );
 }
 
-export default RenameModal;
+export default RenameModal
